16084e12e62ebbb5c5343bf6c8d38cfe5590bd24
Richard Robinson <rrobinson.campos@gmail.com>
2023-08-17 09:53:25 -0300

    feat: Create getMe provider

--- lib/src/core/providers/application_providers.dart
+++ lib/src/core/providers/application_providers.dart
@@ -1,9 +1,11 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../../models/user_model.dart';
 import '../../repositories/user/user_repository.dart';
 import '../../repositories/user/user_repository_impl.dart';
 import '../../services/user_login/user_login_service.dart';
 import '../../services/user_login/user_login_service_impl.dart';
+import '../fp/either.dart';
 import '../http/rest_client.dart';
 
 part 'application_providers.g.dart';
@@ -18,3 +20,14 @@ UserRepository userRepository(UserRepositoryRef ref) =>
 @Riverpod(keepAlive: true)
 UserLoginService userLoginService(UserLoginServiceRef ref) =>
     UserLoginServiceImpl(userRepository: ref.read(userRepositoryProvider));
+
+//* Rievrpod após executar essa função pela primeira vez, os dados ficam em cache sempre tendo a informação disponivel
+@Riverpod(keepAlive: true)
+Future<UserModel> getMe(GetMeRef ref) async {
+  final result = await ref.watch(userRepositoryProvider).me();
+
+  return switch (result) {
+    Success(value: final userModel) => userModel,
+    Failure(:final exception) => throw exception,
+  };
+}
